78eddb143a3cec627ce90624ed6c526e77aa46a5
Mike Sperber <sperber@deinprogramm.de>
2020-09-08 19:30:21 +0200

    Test package.

---
+++ simple-tree-text-markup-test/info.rkt
@@ -0,0 +1,12 @@
+#lang info
+
+(define collection 'multi)
+
+(define deps '("base"))
+(define build-deps '("simple-tree-text-markup-lib"
+                     "rackunit-lib"))
+(define update-implies '("simple-tree-text-markup-lib"))
+
+(define pkg-desc "tests for \"simple-tree-text-markup\"")
+
+(define pkg-authors '(sperber))
---
+++ simple-tree-text-markup-test/simple-tree-text-markup/tests/construct.rkt
@@ -0,0 +1,31 @@
+#lang racket/base
+(require simple-tree-text-markup/construct
+         (only-in simple-tree-text-markup/data vertical-markup)
+         rackunit)
+(check-equal? (horizontal "foo" "bar" "baz")
+              "foobarbaz")
+(check-equal? (horizontal "foo" "bar"
+                          (horizontal "baz" "bla")
+                          "bam" "wup")
+              "foobarbazblabamwup")
+(check-equal? (horizontal "foo" "bar"
+                          (framed-markup "baz")
+                          "bam" "wup")
+              (horizontal "foobar" (framed-markup "baz") "bamwup"))
+
+(check-equal? (vertical)
+              empty-markup)
+(check-equal? (vertical "foo")
+              "foo")
+(check-equal? (vertical "foo")
+              "foo")
+(check-equal? (vertical "foo" empty-markup)
+              "foo")
+(check-equal? (vertical "foo" empty-markup "bar")
+              (vertical "foo" "bar"))
+(check-equal? (vertical "foo" empty-line "bar")
+              (vertical "foo" empty-line "bar"))
+(check-equal? (vertical "foo" "bar")
+              (vertical-markup '("foo" "bar")))
+(check-equal? (vertical "foo" (vertical "bla" "baz") "bar")
+              (vertical "foo" "bla" "baz" "bar"))
