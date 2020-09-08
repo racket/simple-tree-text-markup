0f82501164b8dfca13d8c3839bf649658d1e7b4e
Mike Sperber <sperber@deinprogramm.de>
2020-09-08 19:35:36 +0200

    Parent package.

---
+++ simple-tree-text-markup/info.rkt
@@ -0,0 +1,12 @@
+#lang info
+
+(define collection 'multi)
+
+(define deps '("simple-tree-text-markup-lib"
+               "simple-tree-text-markup-doc"))
+(define implies '("simple-tree-text-markup-lib"
+                  "simple-tree-text-markup-doc"))
+
+(define pkg-desc "Combinator library for simple markup, mainly for displaying messages in a REPL")
+
+(define pkg-authors '(sperber))
