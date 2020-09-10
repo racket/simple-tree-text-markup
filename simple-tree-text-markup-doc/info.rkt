a3473e484528d8dcf269ed5d85aed90866cca71e
Mike Sperber <sperber@deinprogramm.de>
2020-09-10 09:30:38 +0200

    Add dependency on simple-tree-text-markup-lib to simple-tree-text-markup-doc.

--- simple-tree-text-markup-doc/info.rkt
+++ simple-tree-text-markup-doc/info.rkt
@@ -5,7 +5,8 @@
 (define build-deps '("scheme-lib"
                      "at-exp-lib"
                      "scribble-lib"
-                     "racket-doc"))
+                     "racket-doc"
+                     "simple-tree-text-markup-lib"))
 (define deps '("base"))
 
 (define pkg-desc "documentation part of \"simple-tree-text-markup\"")
