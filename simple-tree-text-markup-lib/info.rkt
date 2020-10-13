32a6bb2db6b1313fe94b7001d384effc0a0c0ae7
Mike Sperber <sperber@deinprogramm.de>
2020-10-13 08:26:21 +0200

    Add dependency info, description.

--- simple-tree-text-markup-lib/info.rkt
+++ simple-tree-text-markup-lib/info.rkt
@@ -1,2 +1,10 @@
 #lang info
+
 (define collection 'multi)
+
+(define build-deps '())
+(define deps '("base"))
+
+(define pkg-desc "Simple tree-based markup representation for use in the REPL")
+
+(define pkg-authors '(sperber))
